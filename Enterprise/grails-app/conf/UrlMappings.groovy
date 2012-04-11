2e57d65b9bc55cb5a0dd977ee636ea571b5a7456
gallan.daniele <gallan.daniele@gmail.com>
2012-04-11 09:30:29 +0000

    show course list as first page

--- Enterprise/grails-app/conf/UrlMappings.groovy
+++ Enterprise/grails-app/conf/UrlMappings.groovy
@@ -7,7 +7,13 @@ class UrlMappings {
 			}
 		}
 
-		"/"(view:"/index")
+        // code lines to change the default homepage
+        // in this way, we will see the default action of CourseController (index)
+        "/"{
+            controller = 'Course'
+        }
+
+		// "/"(view:"/index")
 		"500"(view:'/error')
 	}
 }
